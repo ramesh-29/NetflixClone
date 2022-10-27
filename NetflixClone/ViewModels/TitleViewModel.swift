2e6a81c8cab46d32a4bfb919c842412f6103947b
Ramesh <rameshjakhar199929@gmail.com>
2022-10-28 00:06:47 +0530

    coming soon

---
+++ NetflixClone/ViewModels/TitleViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  TitleViewModel.swift
+//  NetflixClone
+//
+//  Created by Ramesh Vishnoi on 27/10/22.
+//
+
+import Foundation
+
+struct TitleViewModel {
+    let titleName: String
+    let posterURL: String
+}
